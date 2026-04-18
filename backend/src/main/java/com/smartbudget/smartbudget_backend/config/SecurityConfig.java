bcff70047f512f593515d73655a4cb864a0dd2a9
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 21:19:30 +0200

    feat(backend): add SecurityConfig for Spring Security setup with JWT and CORS

--- backend/src/main/java/com/smartbudget/smartbudget_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
 
         return provider;
-    }
+    } 
 
     @Bean
     public PasswordEncoder passwordEncoder() {
