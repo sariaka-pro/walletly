c50808108c94658b4ec438ae0913837c73826b41
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 21:13:54 +0200

    feat(backend): add ExpenseController for expense tracking endpoints

---
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/controller/ExpenseController.java
@@ -0,0 +1,57 @@
+package com.smartbudget.smartbudget_backend.controller; 
+
+import com.smartbudget.smartbudget_backend.service.ExpenseService;
+
+import jakarta.validation.Valid;
+
+import com.smartbudget.smartbudget_backend.model.Expense;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    private ExpenseService expenseService; 
+
+    /// Le constructeur du controller. Permet au controller d'utiliser toutes les méthodes du Service. 
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping("")
+    public List<Expense> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getExpenseById(@PathVariable Long id) {
+        Expense expense = expenseService.getExpenseById(id);
+        return ResponseEntity.ok(expense);
+    }
+
+    @PostMapping("")
+    public Expense createExpense(@Valid @RequestBody Expense expense) { /// @Valid permet de valider que tous les  champs dans Expense sont valides. 
+        return expenseService.createExpense(expense);
+    }
+
+    @PutMapping("/{id}")
+    public Expense updateExpense(@PathVariable Long id, @Valid @RequestBody Expense newExpenseData) {
+        return expenseService.updateExpense(id, newExpenseData);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletExpenses(@PathVariable Long id) {
+        expenseService.deleteExpense(id);
+    }
+
+}
