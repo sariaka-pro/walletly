dd80b1b0b8876a459b926590368d2c220c705a4f
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 21:02:32 +0200

    feat(backend): add CategoryService with business logic

---
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/service/CategoryService.java
@@ -0,0 +1,70 @@
+// On définit le package
+package com.smartbudget.smartbudget_backend.service;
+
+// On importe le Repository pour pouvoir l'utiliser
+import com.smartbudget.smartbudget_backend.repository.CategoryRepository;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+// On importe l'annotation @Service pour dire à Spring que cette classe est un service
+import org.springframework.stereotype.Service;
+
+import com.smartbudget.smartbudget_backend.exception.ErrorMessages;
+import com.smartbudget.smartbudget_backend.exception.ForbiddenException;
+import com.smartbudget.smartbudget_backend.exception.NotFoundException;
+// On importe l'entité Category car on va l'utiliser dans nos méthodes
+import com.smartbudget.smartbudget_backend.model.Category;
+import com.smartbudget.smartbudget_backend.model.User;
+
+import java.util.List;
+
+@Service // On dit à Spring : “Cette classe est un service, gère-la automatiquement.”
+public class CategoryService {
+
+    // 1 - On déclare une variable qui va contenir le repository.
+    private final CategoryRepository categoryRepository;
+
+    // 2- On demande à Spring d'injecter automatiquement le repository
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository; // donc ici, on dit que la variable categoryRepository = au
+                                                      // paramètre du constructeur categoryRepository
+    }
+
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new ForbiddenException("User is not authenticated");
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal == null || !(principal instanceof User)) {
+            throw new ForbiddenException("Invalid user principal");
+        }
+        return (User) principal;
+    }
+
+    // 3- Méthode pour récupérer toutes les catégories
+    public List<Category> getAllCategories() {
+        return categoryRepository.findByUser(getCurrentUser());
+    }
+
+    // 4- Méthode pour créer une nouvelle catégorie ou update si la catégorie existe
+    // déjà (recherche par getById()).
+    public Category createCategory(Category customCategory) {
+        customCategory.setUser(getCurrentUser());
+        return categoryRepository.save(customCategory);
+    }
+
+    // 5. supprimer une catégorie
+    public void deleteCategory(Long id) {
+
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(ErrorMessages.CATEGORY_NOT_FOUND));
+
+        User currentUser = getCurrentUser();
+
+        if (!category.getUser().getId().equals(currentUser.getId())) {
+            throw new ForbiddenException(ErrorMessages.ACCESS_DENIED);
+        }
+        categoryRepository.delete(category);
+    }
+}
