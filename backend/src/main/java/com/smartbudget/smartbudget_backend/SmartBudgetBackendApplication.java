aafccc8fea7f6939e3185a28266610ec48f371d8
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 20:54:52 +0200

    chore(backend): add SmartBudgetBackendApplication main class

---
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/SmartBudgetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartbudget.smartbudget_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBudgetBackendApplication.class, args);
+	}
+
+}
