b4028dbe27f25c42faca9a8fb4620ee25a744acc
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 21:13:41 +0200

    feat(backend): add AuthController for authentication endpoints

---
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.smartbudget.smartbudget_backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartbudget.smartbudget_backend.dto.auth.LoginRequest;
+import com.smartbudget.smartbudget_backend.dto.auth.LoginResponse;
+import com.smartbudget.smartbudget_backend.dto.auth.RegisterRequest;
+import com.smartbudget.smartbudget_backend.service.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+    // 1- injecte AuthService 
+    private final AuthService authService; 
+
+    public AuthController(AuthService authService) {
+        this.authService = authService; 
+    }
+
+    // 2. Créer le endpoint vers "/resister" 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request){
+        authService.register(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("User created successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        
+        LoginResponse response = authService.login(request);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(response);
+    }
+    
+
+}
