4f16b1053bef9854f863238ebcf2ccf47acdb0d8
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 21:12:49 +0200

    feat(backend): add converters for entity transformation

---
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/converter/YearMonthConverter.java
@@ -0,0 +1,20 @@
+package com.smartbudget.smartbudget_backend.converter;
+
+import java.time.YearMonth;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class YearMonthConverter implements AttributeConverter<YearMonth, String>{
+    
+    @Override
+    public String convertToDatabaseColumn(YearMonth attribute) {
+        return attribute != null ? attribute.toString() : null;
+    }
+
+    @Override
+    public YearMonth convertToEntityAttribute(String dbData) {
+        return dbData != null ? YearMonth.parse(dbData) : null;
+    }
+}
