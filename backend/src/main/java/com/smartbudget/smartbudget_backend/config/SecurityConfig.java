6a1de679f775d070dbb62940472e64ddc2d5a9e9
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-18 20:56:55 +0200

    chore(backend): configure Spring Security with JWT

---
+++ backend/src/main/java/com/smartbudget/smartbudget_backend/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.smartbudget.smartbudget_backend.config;
+
+import com.smartbudget.smartbudget_backend.security.JwtAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.authentication.AuthenticationProvider;
+
+import com.smartbudget.smartbudget_backend.security.JwtAuthenticationFilter;
+import com.smartbudget.smartbudget_backend.service.CustomUserDetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomUserDetailsService userDetailsService;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable()) // désactive le CSRF (methode auto pour les app avec sessions et cookies)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfig = new org.springframework.web.cors.CorsConfiguration(); 
+                    corsConfig.setAllowedOrigins(java.util.Arrays.asList(
+                    "http://localhost:3000",
+                        "http://localhost:4200"
+                    ));
+                    corsConfig.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfig.setAllowedHeaders(java.util.Arrays.asList("*"));
+                    corsConfig.setAllowCredentials(true);
+                    corsConfig.setMaxAge(3600L);
+                    return corsConfig;
+                }))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() /// authorise tous les chemins commençant par /auth
+                        .anyRequest().authenticated() /// les autres endpoint doivent être authentifiés.
+                )
+                .exceptionHandling(exception -> exception /// gestion de l'authentication 401 qui échoue. 
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                )
+
+                .authenticationProvider(authenticationProvider())
+
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build(); /// build() car spring applique cette config à toutes les requetes.
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() { // valide les credentials en login (email + password)
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
