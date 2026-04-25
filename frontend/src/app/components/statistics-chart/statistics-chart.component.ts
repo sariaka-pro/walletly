fca06cb471afd0151831ddf3d1af83bac6888f34
sariaka ramanantseheno <srkmalala.ram78@gmail.com>
2026-04-26 01:08:06 +0200

    feat(analytics): add analytics page and visualization components

---
+++ frontend/src/app/components/statistics-chart/statistics-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-chart',
+  imports: [],
+  standalone: true,
+  templateUrl: './statistics-chart.component.html',
+  styleUrl: './statistics-chart.component.css',
+})
+export class StatisticsChartComponent {
+
+}
